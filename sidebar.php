5693e01ac902e6236c16c0ebbd519ef59c6948e4
Sumhappy1608 <50817071+Sumhappy1608@users.noreply.github.com>
2020-06-01 17:51:59 +0700

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,56 @@
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+      <!-- Sidebar - Brand -->
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon rotate-n-15">
+          <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+      </a>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider my-0">
+
+      <!-- Nav Item - Dashboard -->
+      <li class="nav-item">
+        <a class="nav-link" href="index.html">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Dashboard</span></a>
+      </li>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider">
+
+      <!-- Heading -->
+      <div class="sidebar-heading">
+        CHỨC NĂNG
+      </div>
+       <!-- Trang chủ -->
+        <li class="nav-item">
+        <a class="nav-link" href="index.php">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Trang chủ</span></a>
+       </li>
+
+      <!-- Quản lý người dùng -->
+        <li class="nav-item">
+        <a class="nav-link" href="users.php">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Quản lý người dùng</span></a>
+       </li>
+
+       <!-- Quản lý bài viết -->
+       <li class="nav-item">
+        <a class="nav-link" href="posts.php">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Quản lý bài viết</span></a>
+       </li>
+
+       <!-- Quản lý thẻ -->
+       <li class="nav-item">
+        <a class="nav-link" href="tags.php">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Quản lý thẻ</span></a>
+       </li>
+       
+    </ul>
